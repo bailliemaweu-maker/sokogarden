76a57eb8b5acd1021eec60eaf153afc4f587e05e
bailliemaweu-maker <bailliemaweu@gmail.com>
2026-05-07 13:57:50 +0300

    w3w

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div><nav class="navbar navbar-expand-md navbar-light  ">
+                    {/* <!-- brand  --> */}
+                    <a href="/" class="navbar-brand text-warning"> Ulinzi Jewels </a>
+                    <button class="navbar-toggler" data-bs-target="#navbarcollapse" data-bs-toggle="collapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    {/* <!-- a division containing our links  --> */}
+                    <div class="collapse navbar-collapse" id="navbarcollapse">
+                        <div class="navbar-nav ">
+                            <a href="" class="nav-link active  "> Home </a>
+                            <a href="Customdesign.html" class="nav-link active"> Custom Design </a>
+                            <a href="location.html" class="nav-link active"> Location </a>
+                            <a href="contact.html" class="nav-link active">Contact</a>
+                        </div>
+                    </div>
+
+                </nav></div>
   )
 }
 
